4f57b3ba2823b1b8ecb1a42b97b2cf9793ff0bd8
Alexis Sniffer <alexis.sniffer@gmail.com>
2023-09-22 20:55:49 -0500

    plugins de upload y mail

---
+++ config/plugins.ts
@@ -0,0 +1,42 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+          secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          params: {
+            ACL: env('AWS_ACL', 'public-read'),
+            signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+            Bucket: env('DO_SPACE_BUCKET'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_USERNAME'),
+        defaultReplyTo: env('SMTP_USERNAME'),
+      },
+    },
+  },
+})
